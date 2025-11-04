26932f24a2be8101165b588e7db16bb202c87a60
Arun Sai Narla <arunsaiknr@gmail.com>
2025-11-03 22:54:01 -0500

    Modified Looping

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const server = http.createServer(function(req,res){
     if(req.url === '/secretData') res.end("There is no secret data to be shared as this is a scrap project !");
-    res.end("Hello World !");
+    else res.end("Hello World !");
 });
 
 server.listen(3000);
